c5adc7f300c735a88895623f7976018764f87ac3
thatcryptomadame <naomijones.invest@gmail.com>
2025-07-25 16:04:04 +0100

    Use frontend_url from .env for CORS

--- Backend/Todo-app/main.py
+++ Backend/Todo-app/main.py
@@ -6,11 +6,18 @@ from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+frontend_url = os.getenv("frontend_url")
+
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5174", "http://localhost:8000"],
+    allow_origins = [frontend_url, "http://localhost:5173"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"]
